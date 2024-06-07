4d99eda3bb3335cbf792e5df4162cd25b4b3fa63
philipp <philipp.reiter90@gmail.com>
2024-06-07 14:34:59 +0200

    init commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Image } from 'expo-image';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Image
+        source={require('./assets/test.jpg')}
+        style={{width: 200, height: 150}}
+        blurRadius={25}
+      />
     </View>
   );
 }
